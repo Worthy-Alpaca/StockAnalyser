3a88e8ff25a78cd32c647a67f3a69e402ae0a255
Yannic <soccerlsc@web.de>
2020-12-11 20:48:31 +0100

    einpflegen der datei

--- beispiele/Internet Tutorial/python for finance 4.py
+++ beispiele/Internet Tutorial/python for finance 4.py
@@ -5,6 +5,11 @@ Tutorials
 import datetime as dt
 import matplotlib.pyplot as plt     # erlaupt zu plotten
 from matplotlib import style        # so sieht es besser aus
+import mplfinance
+from mplfinance.original_flavor import candlestick_ohlc
+# from mplfinance import candlestick_ohlc
+#from matplotlib.finance import candelstick_ohlc  # Candelsticks
+import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
 
@@ -17,17 +22,26 @@ df = web.DataReader('TSLA', 'yahoo', start, end)        #holt sich daten aus zei
 #df.to_csv('tsla.csv')
 
 #df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()        # 100 Average
-df['38ma'] = df['Adj Close'].rolling(window=38, min_periods=0).mean()          # 38  Average
+#df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()        # 100 Average
+#df['38ma'] = df['Adj Close'].rolling(window=38, min_periods=0).mean()          # 38  Average
 
-print(df.head())
+df_ohlc = df['Adj Close'].resample('10D').ohlc()                           #Daten von Bündeln
+df_volume = df['Volume'].resample('10D').sum()
+
+df_ohlc.reset_index(inplace=True)
+
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
+print(df_ohlc.head)
+
 
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.xaxis_date()
 
-ax1.plot(df.index, df['Adj Close'])
-ax1.plot(df.index, df['100ma'])
-ax1.plot(df.index, df['38ma'])
-ax2.bar(df.index, df['Volume'])
+candlestick_ohlc(ax1, df_ohlc.values, width=4, colorup='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
 
 plt.show()
+
+
