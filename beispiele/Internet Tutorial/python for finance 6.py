a805c8798a0996a97f41d5dfea8dc83230693e85
Yannic <soccerlsc@web.de>
2020-12-12 12:10:43 +0100

    Datei Aktualisiert

--- beispiele/Internet Tutorial/python for finance 6.py
+++ beispiele/Internet Tutorial/python for finance 6.py
@@ -8,53 +8,50 @@ Getting all company pricing data in the S&P 500
 
 """
 import bs4 as bs
-import pickle
-import requests
 import datetime as dt
 import os
-import pandas as pd
-import pandas_datareader.data as web
+from pandas_datareader import data as pdr
+import pickle
+import requests
+import yfinance as yf
+
+yf.pdr_override()
 
 def save_sp500_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    soup = bs.BeautifulSoup(resp.text, 'html.parser')
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table', {'class': 'wikitable sortable'})
     tickers = []
-    for row in table.findAll('tr')[1: ]:
-        ticker = row.findAll('td')[0].text
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text.replace('.', '-')
+        ticker = ticker[:-1]
         tickers.append(ticker)
-    with open ("sp500tickers.pickle","wb") as f:
+    with open("sp500tickers.pickle", "wb") as f:
         pickle.dump(tickers, f)
-
-    print(tickers)  
-
     return tickers
 
-save_sp500_tickers()
 
-
+# save_sp500_tickers()
 def get_data_from_yahoo(reload_sp500=False):
     if reload_sp500:
         tickers = save_sp500_tickers()
     else:
-        with open ("sp500tickers.pickle","rb") as f:
+        with open("sp500tickers.pickle", "rb") as f:
             tickers = pickle.load(f)
-
     if not os.path.exists('stock_dfs'):
-        os.makedirs('stock_dfs')   
-
-    start  = dt.datetime(2000,1,1)
-    end  = dt.datetime.now()
-
+        os.makedirs('stock_dfs')
+    start = dt.datetime(2019, 6, 8)
+    end = dt.datetime.now()
     for ticker in tickers:
-        # just in case your connection breaks, we'd like to save our progress!
+        print(ticker)
         if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-            df = web.DataReader(ticker, 'morningstar', start, end)
+            df = pdr.get_data_yahoo(ticker, start, end)
             df.reset_index(inplace=True)
             df.set_index("Date", inplace=True)
-            df = df.drop("Symbol", axis=1)
             df.to_csv('stock_dfs/{}.csv'.format(ticker))
         else:
             print('Already have {}'.format(ticker))
-         
-get_data_from_yahoo()
+
+
+save_sp500_tickers()
+#get_data_from_yahoo()
