ea7f25b1152ed59aca4b447d07b8b0f031357e84
Yannic <soccerlsc@web.de>
2020-12-12 12:49:29 +0100

    Daten aktualisiert

--- beispiele/Internet Tutorial/python for finance 5.py
+++ beispiele/Internet Tutorial/python for finance 5.py
@@ -9,18 +9,16 @@ import pickle
 import requests
 
 def save_sp500_tickers():
-    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-    soup = bs.BeautifulSoup(resp.text)
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table', {'class': 'wikitable sortable'})
     tickers = []
-    for row in table.findAll('tr')[1: ]:
-        ticker = row.findAll('td')[0].text
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text.replace('.', '-')
+        ticker = ticker[:-1]
         tickers.append(ticker)
-    with open ("sp500tickers.pickle","wb") as f:
+    with open("sp500tickers.pickle", "wb") as f:
         pickle.dump(tickers, f)
-
-    print(tickers)  
-
     return tickers
 
 save_sp500_tickers()
