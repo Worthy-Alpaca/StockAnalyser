b1949ac58a30c05196045944674eadea3a6782de
Yannic <soccerlsc@web.de>
2020-12-16 13:14:33 +0100

    Definitionen hinzugefügt

--- src/Model/Chartanalyse.py
+++ src/Model/Chartanalyse.py
@@ -1,4 +1,30 @@
+"""
+
 ### Hier wird die Chartanalyse stehen
 ##Hier ein weiterer eigentest
 
+16.12.2020
+"""
+
 import pandas
+import yfinance as yf
+from matplotlib import pyplot as plt
+
+import yfinance as yf
+aapl = yf.download('AAPL', '2017-1-1','2019-12-18')
+
+
+# Mögliche aktienchartanalysen von
+# https://de.wikipedia.org/wiki/Technische_Analyse#Technische_Indikatoren
+
+#def macd
+    #https://de.wikipedia.org/wiki/MACD
+
+#def dg
+    #https://de.wikipedia.org/wiki/Gleitender_Durchschnittspreis
+
+#def adx
+    #https://de.wikipedia.org/wiki/Average_Directional_Movement_Index
+
+#def 
+ 
