9d3360974684d1eedcd13f7abae90365a5f3f880
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-12-23 16:02:33 +0100

    removed path

--- src/configDUMMY.py
+++ src/configDUMMY.py
@@ -8,4 +8,4 @@ Copy this file and rename the copy to config.py
 
 # This is the path to the project on your PC
 # @example: C:/Users/Stephan/source/repos/diwi4/
-_path = "C:/Users/Yannic/OneDrive/Dokumente/Technische Hochschule Lübeck/Projekt Digitale Wirtschaft/diwi4/"
+_path = ""
