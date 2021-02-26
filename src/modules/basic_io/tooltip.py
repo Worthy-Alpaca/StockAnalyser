2c01d9fcd31a385e08288e1924f1a4bfd2f05c03
Stephan Schumacher aka Worthy Alpaca <worthyalpaca@gmail.com>
2021-02-26 16:22:54 +0100

    removed comments

--- src/modules/basic_io/tooltip.py
+++ src/modules/basic_io/tooltip.py
@@ -24,9 +24,7 @@ class CreateToolTip(object):
         x, y, cx, cy = self.widget.bbox("insert")
         x += self.widget.winfo_rootx() + 25
         y += self.widget.winfo_rooty() + 20
-        # creates a toplevel window
         self.tw = tk.Toplevel(self.widget)
-        # Leaves only the label and removes the app window
         self.tw.wm_overrideredirect(True)
         self.tw.wm_geometry("+%d+%d" % (x, y))
         label = tk.Text(self.tw, height=5, width=70,
